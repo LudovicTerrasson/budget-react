c3ea4165e055a19671f9a868d7f13cc78700a616
marc <chengmarc92@outlook.com>
2024-11-29 17:26:29 +0100

    Ajout des recettes sur le barChat, renommage des label, et changement de couleurs

--- src/components/DashboardPage.js
+++ src/components/DashboardPage.js
@@ -11,10 +11,12 @@ export default function DashboardPage() {
   const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
   const [expensesByCategory, setExpensesByCategory] = useState({});
+  const [recettesByCategory, setRecettesByCategory] = useState({});
+
 
 
   useEffect(() => {
-    const fetchBudgetsAndExpenses = async () => {
+    const fetchBudgetsAndExpensesAndRecettes = async () => {
       try {
         if (user && user.id) {
           const response = await fetch(`http://localhost:8080/budget/getByStudentId/${user.id}`);
@@ -40,6 +42,16 @@ export default function DashboardPage() {
             console.error("Erreur lors de la récupération des dépenses :", dataDepenses);
             setExpensesByCategory({}); // Vider les dépenses par catégorie si erreur
           }
+
+          // Récupérer les recettes
+          const responseRecettes = await fetch(`http://localhost:8080/recette/getRecette/${user.id}`);
+          const dataRecettes = await responseRecettes.json();
+
+          if (responseRecettes.ok) {
+            calculateRecettesByCategory(dataRecettes);
+          } else {
+            setRecettesByCategory({});
+          }
         }
       } catch (error) {
         console.error("Erreur de requête :", error);
@@ -48,7 +60,7 @@ export default function DashboardPage() {
       }
     };
 
-    fetchBudgetsAndExpenses();
+    fetchBudgetsAndExpensesAndRecettes();
   }, [user]);
 
   const calculateTotalsBudget = (budget) => {
@@ -106,13 +118,61 @@ export default function DashboardPage() {
     setExpensesByCategory(sumsByCategory);
   };
 
+  // Fonction pour calculer les recettes par catégorie
+const calculateRecettesByCategory = (recettes) => {
+  const sumsByCategory = {
+    Courses: 0,
+    Housing: 0,
+    Leisure: 0,
+    Subscription: 0,
+    Transport: 0,
+    Savings: 0,
+  };
+
+  recettes.forEach((recette) => {
+    switch (recette.categorie) {
+      case 'coursesBudget':
+        sumsByCategory.Courses += recette.montant;
+        break;
+      case 'housingBudget':
+        sumsByCategory.Housing += recette.montant;
+        break;
+      case 'leisureBudget':
+        sumsByCategory.Leisure += recette.montant;
+        break;
+      case 'subscriptionBudget':
+        sumsByCategory.Subscription += recette.montant;
+        break;
+      case 'transportBudget':
+        sumsByCategory.Transport += recette.montant;
+        break;
+      case 'savingsBudget':
+        sumsByCategory.Savings += recette.montant;
+        break;
+      default:
+        break;
+    }
+  });
+
+  setRecettesByCategory(sumsByCategory);
+  };
   
+  const categoryNames = {
+    Courses: "Courses",
+    Housing: "Logement",
+    Leisure: "Loisirs",
+    Subscription: "Abonnement",
+    Transport: "Transport",
+    Savings: "Épargne",
+  };
 
   // Préparer les données pour le BarChart
   const categories = Object.keys(totalsBudget);
+  const categoriesInFrench = categories.map((category) => categoryNames[category]);
+
   const budgets = categories.map((category) => totalsBudget[category] || 0); // Les montants de budget par catégorie
   const expenses = categories.map((category) => expensesByCategory[category] || 0); // Les montants de dépenses par catégorie
-
+  const recettes = categories.map((category) => recettesByCategory[category] || 0);
 
   return (
     <Container>
@@ -135,7 +195,7 @@ export default function DashboardPage() {
           <Grid container spacing={2}>
             <Grid item xs={12}>
               <Typography variant="h5" gutterBottom>
-                Détails des Budgets
+                Détails des budgets
               </Typography>
               <table style={{ width: '100%', borderCollapse: 'collapse' }}>
                 <thead>
@@ -147,7 +207,7 @@ export default function DashboardPage() {
                 <tbody>
                   {Object.entries(totalsBudget).map(([category, amount]) => (
                     <tr key={category}>
-                      <td style={{ border: '1px solid #ccc', padding: '8px' }}>{category}</td>
+                      <td style={{ border: '1px solid #ccc', padding: '8px' }}>{categoryNames[category] }</td>
                       <td style={{ border: '1px solid #ccc', padding: '8px' }}>{amount.toFixed(2)}</td>
                     </tr>
                   ))}
@@ -161,15 +221,16 @@ export default function DashboardPage() {
                 Avancée des dépenses par catégorie
               </Typography>
               <BarChart
-                xAxis={[{ scaleType: 'band', data: categories }]}// Catégories en axe X
+                className="custom-bar-chart"
+                xAxis={[{ scaleType: 'band', data: categoriesInFrench }]}// Catégories en axe X
                 yAxis={[
-                  {
-                    label: 'Montant (€)', 
+                  {label: 'Montant (€)',       
                   }
                 ]}
                 series={[
-                  { name: 'Dépenses', data: expenses, color: '#82ca9d', label: 'Depense' }, // Série des dépenses
-                  { name: 'Budget', data: budgets, color: '#8884d8' , label: 'Budget'}, // Série des budgets
+                  { name: 'Dépenses', data: expenses, color: '#ff6347', label: 'Depense',stack: false }, // Série des dépenses
+                  { name: 'Budget', data: budgets, color: '#8884d8' , label: 'Budget initial',stack: true}, //série des budgets
+                  { name: 'Recettes', data: recettes, color: '#82ca9d', label: 'Recette',stack: true, }, // Série des recettes
                 ]}
                 height={400} 
                 margin={{ left: 75 }}
